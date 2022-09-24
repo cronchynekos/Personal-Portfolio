deffba8b756dc816d1e56fd0bbb1521e18b4a104
Nathanael Kim <nateykim@gmail.com>
2022-09-23 23:55:01 -0700

    Update index.js

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -3,9 +3,13 @@ import './index.scss';
 
 const Lay = () => {
     return (
-        <>
+        <div className="App">
             <Sidebar />
-        </>
+            <div className="page">
+                <span className="tags top-tags">&lt;body&gt;</span>
+
+            </div>
+        </div>
     )
 }
 
