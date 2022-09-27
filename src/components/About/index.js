91b0c2b002a803533278f0cdbb3454b6bb1f4b08
Nathanael Kim <nateykim@gmail.com>
2022-09-27 01:17:37 -0700

    Added About Me section

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -1,3 +1,25 @@
-const About = () => 'Hello'
+import './index.scss'
+
+const About = () => {
+
+    return(
+        <div className='container about-page'>
+            <div className='text-zone'>
+                <h1>
+                    About Me
+                </h1>
+                <p>
+                    about
+                </p>
+                <p>
+                    about
+                </p>
+                <p>
+                    about
+                </p>
+            </div>
+        </div>
+    )
+}
 
 export default About
