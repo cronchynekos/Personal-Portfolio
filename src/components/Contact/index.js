55143f6c299f8b87bc64aa02c28ac46f57eb7fb2
Nathanael Kim <nateykim@gmail.com>
2022-09-27 19:53:44 -0700

    Fixed contact map

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,6 +1,32 @@
 import './index.scss'
+import { useRef } from 'react'
+import emailjs from '@emailjs/browser'
+import { MapContainer, TileLayer, Popup, Marker } from 'react-leaflet'
+
 
 const Contact = () => {
+
+    const refForm = useRef()
+    const sendEmail = (e) => {
+        e.preventDefault()
+
+        emailjs
+            .sendForm(
+                'gmail',
+                'template_zz27wxh',
+                refForm.current,
+                'xemcS10KHUKkZoDgZaser'
+            )
+            .then(
+                () => {
+                    alert('Message successfully sent!')
+                    window.location.reload(false)
+                },
+                () => {
+                    alert('Failed to send the message, please try again')
+                }
+            )
+    }
     
     return (
         <>
@@ -10,10 +36,10 @@ const Contact = () => {
                         Contact
                     </h1>
                     <p>
-                        contact
+                        Feel free to contact me using email, discord or any of my social medias
                     </p>
                     <div className='contact-form'>
-                        <form>
+                        <form ref={refForm} onSubmit={sendEmail}>
                             <ul>
                                 <li className='half'>
                                     <input 
@@ -34,7 +60,7 @@ const Contact = () => {
                                 <li>
                                     <input 
                                     placeholder='Subject' 
-                                    text="text" 
+                                    type="text" 
                                     name="subject" 
                                     required/>
                                 </li>
@@ -54,6 +80,29 @@ const Contact = () => {
                             </ul>
                         </form>
                     </div>
+                </div>
+                <div className="info-map">
+                    Nathanael Kim
+                    <br/>
+                    California, United States,
+                    <br />
+                    2404 Nutwood Ave
+                    <br />
+                    Fullerton
+                    <br />
+                    nateykim@gmail.com
+                </div>
+                <div className="map-wrap">
+                    <MapContainer center={[33.8768860, -117.8887704]} zoom={13}>
+                        <TileLayer 
+                        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+                        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+                        <Marker position={[33.8768860, -117.8887704]}>
+                            <Popup>
+                            My apartment across <br /> from Cal State Fullerton
+                            </Popup>
+                        </Marker>
+                    </MapContainer>
                 </div>
             </div>
         </>
