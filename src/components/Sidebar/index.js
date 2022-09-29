26fbf2627e98774f441dcc459060dc5a9ae952e8
Nathanael Kim <nateykim@gmail.com>
2022-09-29 04:20:22 -0700

    styling

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -13,13 +13,24 @@ const Sidebar = () => (
             <img className ="sub-logo" src ={LogoSubtitle} alt="Nathan Kim" />
         </Link>
         <nav>
-            <NavLink exact="true" activeclassname="active" to="/">
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            to="/">
                 <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="about-link" to="/about">
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="about-link" 
+            to="/about">
                 <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
             </NavLink>
-            <NavLink exact="true" activeclassname="active" className="contact-link" to="/contact">
+            <NavLink 
+            exact="true" 
+            activeclassname="active" 
+            className="contact-link" 
+            to="/contact">
                 <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
             </NavLink>
         </nav>
