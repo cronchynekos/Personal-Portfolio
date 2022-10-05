2c5e87228e8f5e38e73a37804c89ed0aefeec55b
Nathanael Kim <nateykim@gmail.com>
2022-10-04 21:15:57 -0700

    Added groundwork for 3js integration

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,11 +1,26 @@
 import { Link } from 'react-router-dom' 
 import './index.scss';
+import VoxelLoader from '../Voxel/voxel-loader'
+import dynamic from 'next/dynamic'
+import { Container } from '@chakra-ui/react'
+// import '../Voxel/simpleVox'
+
+
+// const Voxel = dynamic(() => import('../Voxel/voxel-me'), {
+//     ssr: false,
+//     loading: () => <VoxelLoader />
+//   })
 
 const Home = () => {
 
-
     return (
-        <div className ="container home-page">
+        <div className = "container home-page">
+             {/* <Container maxW="container.md" pt={14}>
+
+                 <Voxel />
+
+             </Container> */}
+
             <div className="text-zone">
                 <h1>Hey There, 
                     <br />
@@ -18,6 +33,7 @@ const Home = () => {
                 <Link to="/contact" className='flat-button'>CONTACT ME</Link>
             </div>
         </div>
+
     )
 }
 
