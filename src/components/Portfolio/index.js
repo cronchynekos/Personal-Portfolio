3161e52e8d6d98325b72e12bdbc3999d3f0ffc22
Nathanael Kim <nateykim@gmail.com>
2022-11-09 15:47:20 -0800

    commit

--- src/components/Portfolio/index.js
+++ src/components/Portfolio/index.js
@@ -3,11 +3,44 @@ import "./index.scss";
 import portfolioData from "../../Data/portfolio.json";
 
 const Portfolio = () => {
-    console.log(portfolioData);
+    // console.log(portfolioData);
+
+
+
+    const renderPortfolio = (portfolio) => {
+        return (
+            <div className="images-container"> 
+                {
+                    portfolio.map((port, idx) => {
+                        return (
+                            <div className="image-box" key={idx}>
+                                <img 
+                                width="300" 
+                                height="300"
+                                src={port.cover}
+                                className="portfolio-image"
+                                alt="portfolio" />
+                                <div className="content">
+                                    <p className="title">{port.title}</p>
+                                    <h4 className="description">{port.description}</h4>
+                                    <button
+                                        className="btn"
+                                        onClick={() => window.open(port.url)}>
+                                            View
+                                        </button>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
+        );
+    }
+
     return (
         <div className="container portfolio-page">
             <h1 className="page-title">Portfolio</h1>
-            {/* <div>{renderPortfolio()}</div> */}
+            <div>{renderPortfolio(portfolioData.portfolio)}</div>
         </div>
     )
 }
