668d350c762e6fefabc88a27533190fe75c235fe
Nathanael Kim <nateykim@gmail.com>
2022-11-22 15:16:23 -0800

    Skills Bar

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -23,6 +23,60 @@ const About = () => {
                     tennis, pickleball, basketball and badminton.
                 </p>
             </div>
+
+
+            <div className="cont">
+            <h1 className="title-text">Skills</h1>
+
+            <div className="skill-box">
+                <span className="title">HTML</span>
+                <div className="skill-bar">
+                    <span className="skill-per html">
+                        <span className="tooltip">95%</span>
+                    </span>
+                </div>
+            </div>
+            <div className="skill-box">
+                <span className="title">CSS</span>
+                <div className="skill-bar">
+                    <span className="skill-per css">
+                        <span className="tooltip">80%</span>
+                    </span>
+                </div>
+            </div>
+            <div className="skill-box">
+                <span className="title">JavaScript</span>
+                <div className="skill-bar">
+                    <span className="skill-per javascript">
+                        <span className="tooltip">60%</span>
+                    </span>
+                </div>
+            </div>
+            <div className="skill-box">
+                <span className="title">NodeJS</span>
+                <div className="skill-bar">
+                    <span className="skill-per nodejs">
+                        <span className="tooltip">40%</span>
+                    </span>
+                </div>
+            </div>
+            <div className="skill-box">
+                <span className="title">ReactJS</span>
+                <div className="skill-bar">
+                    <span className="skill-per reactjs">
+                        <span className="tooltip">70%</span>
+                    </span>
+                </div>
+            </div>
+            <div className="skill-box">
+                <span className="title">ExpressJS</span>
+                <div className="skill-bar">
+                    <span className="skill-per expressjs">
+                        <span className="tooltip">75%</span>
+                    </span>
+                </div>
+            </div>
+        </div>
         </div>
     )
 }
